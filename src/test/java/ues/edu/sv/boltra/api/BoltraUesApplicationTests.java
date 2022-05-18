f0da0a4c523822e57edaf81341d6fecaaf5013aa
Cesar Gomez <mg18045@ues.edu.sv>
2022-05-17 21:42:12 -0600

    Initial Commit

---
+++ src/test/java/ues/edu/sv/boltra/api/BoltraUesApplicationTests.java
@@ -0,0 +1,13 @@
+package ues.edu.sv.boltra.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoltraUesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
